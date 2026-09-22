583d4fdcf6a8407f9a9fa80aeca079559726f964
Fyonietz <expduta@gmail.com>
2026-09-22 10:32:41 +0700

    feat(Core):Update Build Zig Become Flexible

--- build.zig
+++ build.zig
@@ -4,19 +4,29 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "Parser",
-        .root_module = b.createModule(.{
-            .optimize = optimize,
-            .target = target,
-            .root_source_file = b.path("src/main.zig")
-        })
-    });
+    const folders = .{
+        .{ "src", "src" },
+        .{ "calc", "calc" },
+    };
 
-    b.installArtifact(exe);
-    
-    const run_step = b.step("run","running the app");
+    inline for (folders) |entry| {
+        const folder = entry[0];
+        const step_name = entry[1];
+        const main_path = folder ++ "/main.zig";
 
-    const run_cmd = b.addRunArtifact(exe);
-    run_step.dependOn(&run_cmd.step);
+        const exe = b.addExecutable(.{
+            .name = folder,
+            .root_module = b.createModule(.{
+                .optimize = optimize,
+                .target = target,
+                .root_source_file = b.path(main_path),
+            }),
+        });
+
+        b.installArtifact(exe);
+
+        const run_cmd = b.addRunArtifact(exe);
+        const run_step = b.step(step_name, b.fmt("Run {s}", .{folder}));
+        run_step.dependOn(&run_cmd.step);
+    }
 }
